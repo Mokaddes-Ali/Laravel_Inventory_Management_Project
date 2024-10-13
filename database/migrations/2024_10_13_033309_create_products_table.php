e56964e8f411d450a6b2131984d6c22771575b98
mokadde <mokaddes.ru2000@gmail.com>
2024-10-13 09:37:13 +0600

    commit

--- database/migrations/2024_10_13_033309_create_products_table.php
+++ database/migrations/2024_10_13_033309_create_products_table.php
@@ -11,18 +11,35 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('brands', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
+
             
             $table->id();
-            $table->string('brandName',50);
-            $table->string('brandImg',300)->nullable();
             $table->unsignedBigInteger('creator');
             $table->unsignedBigInteger('editor')->nullable();
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('brand_id');
+
 
             $table->foreign('creator')->references('id')->on('users')
                 ->cascadeOnUpdate()->restrictOnDelete();
                 $table->foreign('editor')->references('id')->on('users')
                 ->cascadeOnUpdate()->restrictOnDelete();
+            $table->foreign('category_id')->references('id')->on('categories')
+                ->cascadeOnUpdate()->restrictOnDelete();
+
+            $table->foreign('brand_id')->references('id')->on('brands')
+                ->restrictOnDelete()
+                ->cascadeOnUpdate();
+            $table->string('name',100);
+            $table->string('code')->nullable()->unique();
+            $table->string('price',50);
+            $table->string('cost',50);
+            $table->string('unit',50);
+            $table->string('img_url',100);
+            $table->text('details')->nullable();
+            $table->string('slug',50)->nullable();
+            $table->integer('status')->default(1);
 
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
@@ -35,6 +52,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('brands');
+        Schema::dropIfExists('products');
     }
 };
--- database/migrations/2024_10_13_033546_create_invoices_table.php
+++ database/migrations/2024_10_13_033546_create_invoices_table.php
@@ -11,18 +11,25 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('brands', function (Blueprint $table) {
-            
+        Schema::create('invoices', function (Blueprint $table) {
             $table->id();
-            $table->string('brandName',50);
-            $table->string('brandImg',300)->nullable();
-            $table->unsignedBigInteger('creator');
-            $table->unsignedBigInteger('editor')->nullable();
 
-            $table->foreign('creator')->references('id')->on('users')
+
+            $table->string('total',50);
+            $table->string('discount',50);
+            $table->string('vat',50);
+            $table->string('payable',50);
+
+
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('customer_id');
+
+            $table->foreign('user_id')->references('id')->on('users')
                 ->cascadeOnUpdate()->restrictOnDelete();
-                $table->foreign('editor')->references('id')->on('users')
+
+            $table->foreign('customer_id')->references('id')->on('customers')
                 ->cascadeOnUpdate()->restrictOnDelete();
+
 
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
@@ -35,6 +42,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('brands');
+        Schema::dropIfExists('invoices');
     }
 };

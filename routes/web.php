ae67a54ed2b84d6231238d7a6c4bde416fce4aec
mokadde <mokaddes.ru2000@gmail.com>
2024-10-16 07:12:17 +0600

    commit

--- routes/web.php
+++ routes/web.php
@@ -46,67 +46,13 @@ Route::middleware('auth')->group(function () {
 
 
 Route::middleware('auth')->group(function () {
-
 Route::get('/products', [ProductController::class, 'index']);
-
 Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-
-
 Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
 Route::get('/products/edit/{id}', [ProductController::class, 'edit']);
 Route::put('/products/update/{id}', [ProductController::class, 'update']);
 Route::delete('/products/delete/{id}', [ProductController::class, 'destroy']);
-
-
-
-
 });
-
-
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/client', [ClientController::class, 'index'])->name('index');
-//     Route::post('/client/submit', [ClientController::class, 'store'])->name('store');
-//     Route::get('/client/show', [ClientController::class, 'show'])->name('show');
-//     Route::get('/client/edit/{id}', [ClientController::class, 'edit']);
-//     Route::post('/client/update', [ClientController::class, 'update']);
-//     Route::get('/delete/{id}', [ClientController::class, 'destroy']);
-// });
-
-
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/project', [ProjectController::class, 'index'])->name('index');
-//     Route::post('/project/submit', [ProjectController::class, 'store'])->name('store');
-//     Route::get('/show/project', [ProjectController::class, 'projectshow'])->name('projectshow');
-//     Route::get('/edit/project/{id}', [ProjectController::class, 'projectedit'])->name('projectedit');
-//     Route::post('/project/update', [ProjectController::class, 'projectupdate'])->name('projectupdate');
-//     Route::post('/delete/{id}', [ProjectController::class, 'projectdestroy'])->name('projectdestroy');
-// });
-
-
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/income', [IncomeController::class, 'incomeindex'])->name('incomeindex');
-//     Route::post('/income/submit', [IncomeController::class, 'incomestore'])->name('incomestore');
-//     Route::get('/show/income', [IncomeController::class, 'incomeshow'])->name('incomeshow');
-//     Route::get('/income/edit/{id}', [IncomeController::class, 'edit'])->name('incomeedit');
-//     Route::post('/income/update', [IncomeController::class, 'update'])->name('incomeupdate');
-//     Route::get('/invoice/filter', [IncomeController::class, 'filter'])->name('invoice.filter');
-//     Route::delete('/income/delete/{id}', [IncomeController::class, 'destroy'])->name('income.delete');
-//     Route::get('/invoice/search', [IncomeController::class, 'search'])->name('invoice.search');
-
-// });
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/expense', [ExpenseController::class, 'index'])->name('expenseindex');
-//     Route::post('/expense/submit', [ExpenseController::class, 'store'])->name('expensestore');
-//     Route::get('/show/expense', [ExpenseController::class, 'show'])->name('expenseshow');
-//     Route::get('/expense/edit/{id}', [ExpenseController::class, 'edit'])->name('expenseedit');
-//     Route::post('/expense/update', [ExpenseController::class, 'update'])->name('expenseupdate');
-//     Route::get('/delete/{id}', [ExpenseController::class, 'destroy'])->name('expensedestroy');
-// });
-
 
 
 Route::middleware('auth')->group(function () {
